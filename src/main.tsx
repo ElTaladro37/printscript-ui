747956f2cbbf6cc0ea2da5623b302c42c635906d
AbramowichAna <aabramowich@mail.austral.edu.ar>
2024-10-31 19:11:04 -0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -9,8 +9,8 @@ import {Auth0Provider} from "@auth0/auth0-react";
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Auth0Provider
-            domain={import.meta.env.VITE_AUTH0_DOMAIN ?? ""}
-            clientId={import.meta.env.VITE_AUTH0_CLIENT_ID ?? ""}
+            domain={import.meta.env.VITE_AUTH_SERVER_URI ?? ""}
+            clientId={import.meta.env.VITE_AUTH_CLIENT_ID ?? ""}
             authorizationParams={{
                 redirect_uri: window.location.origin
             }}
