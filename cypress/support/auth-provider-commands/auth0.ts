042dbcf4e22416711b3fd04ff896793d2170bba0
pablo <pbaratta@mail.austral.edu.ar>
2024-11-10 17:22:11 -0300

    tests running

--- cypress/support/auth-provider-commands/auth0.ts
+++ cypress/support/auth-provider-commands/auth0.ts
@@ -1,14 +1,14 @@
 
-
 export function loginViaAuth0Ui(username: string, password: string) {
   // App landing page redirects to Auth0.
   cy.visit('/')
-
+console.log(username + " gg " + password)
   // Login on Auth0.
   cy.origin(
-      Cypress.env('auth0_domain'),
+      "https://dev-yjbbmbvlnk6d2bqm.us.auth0.com",
       { args: { username, password } },
       ({ username, password }) => {
+          console.log(username + password)
         cy.get('input#username').type(username)
         cy.get('input#password').type(password, { log: false })
         cy.contains('button[value=default]', 'Continue').click()
@@ -16,7 +16,7 @@ export function loginViaAuth0Ui(username: string, password: string) {
   )
 
   // Ensure Auth0 has redirected us back to the RWA.
-  cy.url().should('equal', 'http://localhost:3000/')
+  cy.url().should('equal', "http://localhost:5173/")
 }
 
 
