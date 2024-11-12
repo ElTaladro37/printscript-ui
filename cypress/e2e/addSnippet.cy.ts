a3cb71ccdef6f152abbdb60af0cbac9e5005257e
Josefina Sapag <jmsapag17@gmail.com>
2024-11-12 00:13:51 -0300

    feat: trying to make tests

--- cypress/e2e/addSnippet.cy.ts
+++ cypress/e2e/addSnippet.cy.ts
@@ -11,8 +11,7 @@ describe('Add snippet tests', () => {
     cy.visit("/")
     cy.intercept('POST', BACKEND_URL+"/snippet/text", (req) => {
       req.reply((res) => {
-        // expect(res.body).to.include.keys("fileUrl", "snippetId", "languageId", "versionId")
-        console
+        expect(res.body).to.include.keys("fileUrl", "snippetId", "languageId", "versionId")
         expect(res.statusCode).to.eq(200);
       });
     }).as('postRequest');
@@ -32,19 +31,29 @@ describe('Add snippet tests', () => {
   })
 
   it('Can add snippets via file', () => {
-    cy.visit("/")
-    cy.intercept('POST', BACKEND_URL+"/snippets", (req) => {
+    cy.visit("/");
+
+    // Intercept the POST request and validate the response
+    cy.intercept('POST', BACKEND_URL + "/snippet/text", (req) => {
       req.reply((res) => {
-        expect(res.body).to.include.keys("name", "description", "language", "version", "snippetFile")
+        expect(res.body).to.include.keys("fileUrl", "snippetId", "languageId", "versionId");
         expect(res.statusCode).to.eq(200);
       });
     }).as('postRequest');
 
-    /* ==== Generated with Cypress Studio ==== */
-    cy.get('[data-testid="upload-file-input"]').selectFile("cypress/fixtures/example_ps.ps", {force: true})
+    // Open the menu to select "Load snippet from file"
+    cy.get('.css-9jay18 > .MuiButton-root').click();
+    cy.get('.MuiList-root > [tabindex="-1"]').should('be.visible').click();
 
-    cy.get('[data-testid="SaveIcon"]').click();
+    // Upload the file directly to the input element
+    cy.get('[data-testid="upload-file-input"]').should('exist').selectFile('cypress/fixtures/example.prs', { force: true });
 
+    // Click the Save button to submit
+    cy.get('[data-testid="SaveIcon"]').should('be.visible').click();
+
+    // Wait for the POST request to complete and validate the status code
     cy.wait('@postRequest').its('response.statusCode').should('eq', 200);
-  })
+  });
+
+
 })
